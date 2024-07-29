bda5350d137f989f9d142c1619e1497b243e363a
Harshaljain04 <harshaljn04@gmail.com>
2024-07-29 23:32:35 +0530

    drag and drop added

--- DragNDrop/main.py
+++ DragNDrop/main.py
@@ -19,12 +19,13 @@ while True:
         
         l, _, _ =detector.findDistance(8,12,img)
         print(l)
-        cursor = lmList[8]
-        if cx-w//2<cursor[0]<cx+w//2 and cy-h//2<cursor[1]<cy+h//2:
-            colorR = 0,255,0
-            cx,cy = cursor
-        else:
-            colorR = 255,0,255
+        if l<30:
+            cursor = lmList[8]
+            if cx-w//2<cursor[0]<cx+w//2 and cy-h//2<cursor[1]<cy+h//2:
+                colorR = 0,255,0
+                cx,cy = cursor
+            else:
+                colorR = 255,0,255
     
     cv2.rectangle(img, (cx-w//2,cy-h//2), (cx+w//2,cy+h//2), colorR, cv2.FILLED)
     cv2.imshow("Image", img)
