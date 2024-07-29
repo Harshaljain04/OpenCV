145818b9edc79df35d555d4b8ede598ee1253fb4
Harshaljain04 <harshaljn04@gmail.com>
2024-07-29 21:54:04 +0530

    29-07-24

--- DragNDrop/main.py
+++ DragNDrop/main.py
@@ -7,6 +7,8 @@ cap.set(4, 720)
 detector = HandDetector(detectionCon=0)
 colorR = 255,0,255
 
+cx,cy,w,h = 100,100,200,200 
+
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
@@ -14,12 +16,16 @@ while True:
     lmList, _ = detector.findPosition(img)
     
     if lmList:
+        
+        l, _, _ =detector.findDistance(8,12,img)
+        
         cursor = lmList[8]
-        if 100<cursor[0]<300 and 100<cursor[1]<300:
+        if cx-w//2<cursor[0]<cx+w//2 and cy-h//2<cursor[1]<cy+h//2:
             colorR = 0,255,0
+            cx,cy = cursor
         else:
             colorR = 255,0,255
     
-    cv2.rectangle(img, (100,100), (300,300), colorR, cv2.FILLED)
+    cv2.rectangle(img, (cx-w//2,cy-h//2), (cx+w//2,cy+h//2), colorR, cv2.FILLED)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
